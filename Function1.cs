b7b8d8e2cc119d5801c486fa2e1a67d81804fcca
archana.stsintern <archana.stsintern@gmail.com>
2024-07-30 14:14:08 +0530

    master_hmacfunction yml file removed

--- Function1.cs
+++ Function1.cs
@@ -18,7 +18,7 @@ namespace AzureHmac
         public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
+            return new OkObjectResult("Welcome to Azure Functions!, I am archana");
         }
     }
 }
